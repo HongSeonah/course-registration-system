3e7674b51ea6549c44bacc3bf278ee491df2c364
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2026-05-21 23:45:57 +0900

    docs: todo 제거 (기존 방식 유지)

--- src/main/java/com/example/courseregistration/domain/enrollment/dto/request/EnrollmentCreateRequest.java
+++ src/main/java/com/example/courseregistration/domain/enrollment/dto/request/EnrollmentCreateRequest.java
@@ -2,7 +2,6 @@ package com.example.courseregistration.domain.enrollment.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 
-// TODO: record 하나로 간략화
 public record EnrollmentCreateRequest(
         @NotNull Long classmateId
 ) {
